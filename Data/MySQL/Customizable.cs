e433714c0b7b86f3dba073bdbce33699611730d9
himani-dxdy <142305945+himani-dxdy@users.noreply.github.com>
2023-10-10 20:20:55 +0530

    Render Inquiry (Series Group) Step Fields
    Fix display error in acknowledge inquiry
    
    Render checkboxes in order in Inquiry (Series Group) Step

--- Data/MySQL/Customizable.cs
+++ Data/MySQL/Customizable.cs
@@ -107,15 +107,15 @@ namespace N_Ter.MySQL.Customizable
             if (ds.tbltasks[0].Current_Step_ID == 123) // For step 1.2.2 Acknowledge Inquiry (Quotation Status)
             {
                 ret = "init.push(function () {\r\n" +
-                                "CheckCompType();\r\n" +
+                                "CheckQuotationStatus();\r\n" +
                         "});\r\n" +
                         "$('#Field_ID_379').change(function() {\r\n" + // Quotation status selection
-                                "CheckCompType();\r\n" +
+                                "CheckQuotationStatus();\r\n" +
                         "});\r\n" +
                         "$('#Field_ID_380').change(function() {\r\n" + // Confirmation sent checkbox
-                                "CheckCompType();\r\n" +
+                                "CheckConfirmationSent();\r\n" +
                         "});\r\n" +
-                        "function CheckCompType() {\r\n" +
+                        "function CheckQuotationStatus() {\r\n" +
                              "if ($('#Field_ID_379').val() == 'Confirmation of the inquiry'){\r\n" + // Only show the next fields when the quotation status is
                                  "$('#ControlContainer_380').removeClass('hide');\r\n" +             // confirmation of inquiry
                              "}\r\n" +
@@ -123,18 +123,69 @@ namespace N_Ter.MySQL.Customizable
                                  "$('#ControlContainer_380').addClass('hide');\r\n" +
                                  "$('#ControlContainer_381').addClass('hide');\r\n" +
                                  "$('#ControlContainer_382').addClass('hide');\r\n" +
-                             "}\r\n" +
-                              "if ($('#Field_ID_380').is(\":checked\")){\r\n" + // Confirmation sent checkbox
+                             "}\r\n" +   
+                         "}\r\n" +
+                         "function CheckConfirmationSent() {\r\n" +
+                            "if ($('#Field_ID_380').is(\":checked\")){\r\n" + // Confirmation sent checkbox
                                  "$('#ControlContainer_381').removeClass('hide');\r\n" + // Upload amended quotation
                                  "$('#ControlContainer_382').removeClass('hide');\r\n" + // Amend quotation sent checkbox
                               "}\r\n" +
-                               "else {\r\n" +
+                            "else {\r\n" +
                                  "$('#ControlContainer_381').addClass('hide');\r\n" + // Upload amended quotation
                                  "$('#ControlContainer_382').addClass('hide');\r\n" + // Amend quotation sent checkbox
+                            "}\r\n" +
+                          "}\r\n";
+            }
+
+            if (ds.tbltasks[0].Current_Step_ID == 113) // For step 2.2 Inquiry (Series group)
+            {
+                ret = "init.push(function () {\r\n" +
+                                "CheckHotelRatesSent();\r\n" +
+                        "});\r\n" +
+                        "$('#Field_ID_353').change(function() {\r\n" +
+                                "CheckHotelRatesSent();\r\n" +
+                        "});\r\n" +
+                        "$('#Field_ID_354').change(function() {\r\n" +
+                                "CheckAllotmentConfirmed();\r\n" +
+                        "});\r\n" +
+                        "function CheckHotelRatesSent() {\r\n" +
+                            "if ($('#Field_ID_353').is(\":checked\")){\r\n" + // Hotel rates sent to client checkbox
+                                "$('#ControlContainer_354').removeClass('hide');\r\n" + // Allotment confirmed checkbox
+                                 "$('#ControlContainer_355').addClass('hide');\r\n" + // Hotel rates confirmed checkbox
+                                "$('#ControlContainer_361').addClass('hide');\r\n" + // Costing amount textbox
+                                "$('#ControlContainer_356').addClass('hide');\r\n" + // Costing updated checkbox
+                                "$('#ControlContainer_357').addClass('hide');\r\n" + // Tour operator confirmed checkbox
+                                "$('#ControlContainer_358').addClass('hide');\r\n" + // Sent hotel voucher checkbox
+                            "}\r\n" +
+                            "else {\r\n" +
+                               "$('#ControlContainer_354').addClass('hide');\r\n" + // Allotment confirmed checkbox
+                                "$('#ControlContainer_355').addClass('hide');\r\n" + // Hotel rates confirmed checkbox
+                                "$('#ControlContainer_361').addClass('hide');\r\n" + // Costing amount textbox
+                                "$('#ControlContainer_356').addClass('hide');\r\n" + // Costing updated checkbox
+                                "$('#ControlContainer_357').addClass('hide');\r\n" + // Tour operator confirmed checkbox
+                                "$('#ControlContainer_358').addClass('hide');\r\n" + // Sent hotel voucher checkbox
+                            "}\r\n" +
+                        "}\r\n" +
+                         "function CheckAllotmentConfirmed() {\r\n" +
+                             "if ($('#Field_ID_354').is(\":checked\")){\r\n" +
+                                 "$('#ControlContainer_355').removeClass('hide');\r\n" +
+                                 "$('#ControlContainer_361').addClass('hide');\r\n" + // Costing amount textbox
+                                 "$('#ControlContainer_356').addClass('hide');\r\n" + // Costing updated checkbox
+                                 "$('#ControlContainer_357').addClass('hide');\r\n" + // Tour operator confirmed checkbox
+                                 "$('#ControlContainer_358').addClass('hide');\r\n" + // Sent hotel voucher checkbox
+                             "}\r\n" +
+                             "else {\r\n" +
+                                 "$('#ControlContainer_355').addClass('hide');\r\n" +
+                                 "$('#ControlContainer_361').addClass('hide');\r\n" + // Costing amount textbox
+                                 "$('#ControlContainer_356').addClass('hide');\r\n" + // Costing updated checkbox
+                                 "$('#ControlContainer_357').addClass('hide');\r\n" + // Tour operator confirmed checkbox
+                                 "$('#ControlContainer_358').addClass('hide');\r\n" + // Sent hotel voucher checkbox
                              "}\r\n" +
                          "}\r\n";
-            }
-            return ret;
+            };
+
+
+                return ret;
         }
 
         /// <summary>
